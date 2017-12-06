a694a6de99952dfff06741cc7a11a4f82597ad08
opulentj <33817518+opulentj@users.noreply.github.com>
2017-12-05 20:37:14 -0500

    Update FortuneTeller.java

--- FortuneTeller.java
+++ FortuneTeller.java
@@ -1,5 +1,45 @@
+Import java.util.Scanner;
+
 public class FortuneTeller{
-  public static void main(String[] args){
-  
-  }
+
+public static void main(String[]args){
+  Scanner scanner = new Scanner (System.in);
+
+  System.out.printf("%nWelcome to the Virtual Paper Fortune Teller!%n What is your name: ");
+
+     String name = scanner.nextLine();
+
+     boolean play = false;
+     System.out.printf ("Hello %s, Are you ready to play? Please enter Yes or No ",name);
+     String gameAns = scanner.nextLine();
+     System.out.println("Please enter 4 colors with spaces: ");
+     String userColors = scanner.nextLine().trim();
+     String[] colors = userColors.split(" ");
+     int i = 0;
+     System.out.println("The colors you chose are: ");
+     while(i < colors.length) {
+     System.out.println(colors[i++]);
+
+     }
+     int chosenColor =  pickColor(colors);
+     System.out.println(chosenColor);
+
 }
+     public static int pickColor (String [] colors) {
+      Scanner scanner = new Scanner (System.in);
+      System.out.println("Pick one of your colors: ");
+      String oneColor = scanner.nextLine();
+      int length = 0;
+      for (int i = 0; i < colors.length; i++){
+       if (oneColor.equals(colors[i]) ) {
+          length = oneColor.length();
+
+       }
+
+    }
+    return length;
+
+  }
+
+}
+
